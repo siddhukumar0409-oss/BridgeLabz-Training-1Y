8a912cd9a41b43c875774406e6b2ffeadf1b6312
BriddgeLabz-Training-1Y <Siddhukumar0409>
2026-01-14 20:59:44 +0530

    Program of To Find Area of Circle

---
+++ basic-programs/Area_of_a_Circle.java
@@ -0,0 +1,10 @@
+
+import java.util.Scanner;
+public class Area_of_a_Circle {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print(" Enter radius of circle :");
+      double radius=sc.nextInt();
+      double Area=(3.1415*radius*radius);
+            System.out.print("The area of circle : " +Area);
+}}
