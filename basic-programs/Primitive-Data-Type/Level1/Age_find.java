f17ab2aa1ff79e5d180dacfa1e2c97c7b4c72b24
BriddgeLabz-Training-1Y <Siddhukumar0409>
2026-01-21 08:50:45 +0530

    Added: Program For Calculating Harry's Age

---
+++ basic-programs/Primitive-Data-Type/Level1/Age_find.java
@@ -0,0 +1,10 @@
+
+  public class Age_find
+  {
+   public static void main(String[] args){
+int birth= 2000;  // Birth year of harry 
+int year=2024;   //Current year 
+int age =(year-birth);
+System.out.print("Harry's age in 2024 is    " + age  );
+   }
+}
