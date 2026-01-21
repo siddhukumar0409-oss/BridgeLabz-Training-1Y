e4e42e45b2e71bb5c20364f40330d65ca1fd0abc
BriddgeLabz-Training-1Y <Siddhukumar0409>
2026-01-21 21:17:32 +0530

    Average percentage of marks

---
+++ basic-programs/Primitive-Data-Type/Level1/Average_per.java
@@ -0,0 +1,9 @@
+public class Average_per {
+    public static void main(String[] args) {
+        int num1=94; // Marks in 1st subject
+        int num2=95; // Marks in 2st subject
+        int num3=96; // Marks in 3rd subject
+    double averge=(num1+num2+num3)/3;
+    System.out.print("Sam's average mark in PCM is   " + averge );
+    }
+}
