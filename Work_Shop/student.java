25154cad5da35ae8c0b9ce9aca240dcd67516fb8
BriddgeLabz-Training-1Y <Siddhukumar0409>
2026-02-01 13:36:54 +0530

    Programe for Student id

---
+++ Work_Shop/student.java
@@ -0,0 +1,14 @@
+public class student {
+   
+    int age;
+    int id;
+    String name;
+    public static void main(String[] args) {
+
+     student p = new student();
+     p.name="Mahseh";
+        p .age=25;
+        p. id=100;
+        System.out.print("Age is ==> " +p.age+ "  id is  ==> " +p.id+"  and  Name is  ==>  "+p.name);  
+    }
+}
