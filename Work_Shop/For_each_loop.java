5500a04ac696c0d49fb61eed04b54de62dc93e1c
BriddgeLabz-Training-1Y <Siddhukumar0409>
2026-02-01 13:46:41 +0530

    Programe of For_each  loop

---
+++ Work_Shop/For_each_loop.java
@@ -0,0 +1,8 @@
+public class For_each_loop {
+    public static void main(String[] args) {
+        int[] array={10,20,30,40,50};
+        for(int num:array){
+           System.err.println(num); 
+        }
+    }
+}
