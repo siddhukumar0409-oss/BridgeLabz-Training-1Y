62d1f919b3bc02486c6127a55756857e9f33a055
BriddgeLabz-Training-1Y <Siddhukumar0409>
2026-02-01 13:51:09 +0530

    Programe of Do_while  loop

---
+++ Work_Shop/Do_whileloop.java
@@ -0,0 +1,12 @@
+public class Do_whileloop {
+    public static void main(String[] args) {
+        int i=0;
+        do{
+            System.err.println(i);
+            i++;
+        }
+        while(i<=20);{
+            System.err.println(i);
+        }
+
+    }}
