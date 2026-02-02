ad4fe966451aab495b907db886bac7dec5fa8031
BriddgeLabz-Training-1Y <Siddhukumar0409>
2026-02-02 14:53:57 +0530

    Programe of Array example

---
+++ Work_Shop/Array.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Array {
+    public static void main(String[] args) {
+        int [] Marks={50,85,60,70};
+Scanner sc= new Scanner(System.in);
+for(int i=0;i<Marks.length;i++){
+System.out.println(Marks[i]);
+}   }
+
+    
+}
