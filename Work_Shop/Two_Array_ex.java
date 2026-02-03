e2133b9df3323b87b3c287ad8144c80e3f7e9a4f
BriddgeLabz-Training-1Y <Siddhukumar0409>
2026-02-03 23:28:30 +0530

    Programe of 2D array example

---
+++ Work_Shop/Two_Array_ex.java
@@ -0,0 +1,13 @@
+public class Two_Array_ex {
+    public static void main(String[] args) {
+        int [][] marks={
+            {80,90},{70,76}
+        };
+        for(int i=0;i<marks.length;i++){
+for(int j=0;j<marks.length;j++){
+    System.out.println(marks[i][j]);
+}
+        }
+        System.out.println( " ");
+    }
+}
